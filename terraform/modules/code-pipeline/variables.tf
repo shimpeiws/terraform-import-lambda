537b50654585d63ecd2ea4dc221fc99e26f873d3
shimpeiws <shimpeiws@gmail.com>
2022-12-21 06:11:29 +0900

    CodePipeline

---
+++ terraform/modules/code-pipeline/variables.tf
@@ -0,0 +1,44 @@
+variable "code_build_project_name" {
+  description = "CodeBuild Project name"
+  type        = string
+}
+
+variable "code_pipeline_project_name" {
+  description = "CodePipeline Project name"
+  type        = string
+}
+
+variable "github_project_url" {
+  description = "GitHub project URL"
+  type        = string
+}
+
+variable "github_account_name" {
+  description = "GitHub account name or organization name"
+  type        = string
+}
+
+variable "github_oauth_token" {
+  description = "GitHub oauth token"
+  type        = string
+}
+
+variable "github_repo_name" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "github_branch_name" {
+  description = "GitHub branch name"
+  type        = string
+}
+
+variable "build_buildspec_path" {
+  description = "Path for build spec that is used for terraform plan"
+  type        = string
+}
+
+variable "deploy_buildspec_path" {
+  description = "Path for build spec that is used for terraform apply"
+  type        = string
+}
